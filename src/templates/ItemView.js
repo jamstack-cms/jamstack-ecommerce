ff6f5a9348a0ce63e5ebc491ffd65c49b41b1b9e
Steven Almeroth <sroth77@gmail.com>
2020-05-06 23:27:10 -0400

    add image popup

--- src/templates/ItemView.js
+++ src/templates/ItemView.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import Button from '../components/Button'
+import Popup from "reactjs-popup"
 
 import { SiteContext, ContextProviderComponent } from '../context/mainContext'
 import CartLink from '../components/CartLink'
+import Button from '../components/Button'
 import Image from '../components/Image'
 
 const ItemView = (props) => {
@@ -23,7 +24,9 @@ const ItemView = (props) => {
       my-0 mx-auto">
         <div className="w-full md:w-1/2 h-112 flex flex-1 bg-light hover:bg-light-200">
           <div className="py-16 p10 flex flex-1 justify-center items-center">
-            <Image src={image} className="max-w-lg m-0 max-h-96 w-64 md:w-full" alt="Inventory item"  />
+            <Popup modal trigger={<img src={image} className="max-w-lg m-0 max-h-96 w-64 md:w-full" alt="Inventory item" />}>
+              <Image src={image} alt="Inventory item" />
+            </Popup>
           </div>
         </div>
         <div className="pt-2 px-0 md:px-10 pb-8 w-full md:w-1/2">
