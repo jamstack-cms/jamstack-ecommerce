5290b608a61097c25b4cf8f800122b8cc772acea
Joe Ng'ethe <joeynimu@gmail.com>
2021-01-27 20:16:29 +0300

    feature: single sku fetch from backend and only allow quantities available in the inventory

--- pages/product/[name].js
+++ pages/product/[name].js
@@ -1,26 +1,34 @@
-import { useState } from 'react'
-import Head from 'next/head'
-import Button from '../../components/Button'
-import Image from '../../components/Image'
-import QuantityPicker from '../../components/QuantityPicker'
-import { fetchInventory } from '../../utils/inventoryProvider'
-import { slugify } from '../../utils/helpers'
-import CartLink from '../../components/CartLink'
-import { SiteContext, ContextProviderComponent } from '../../context/mainContext'
+import { useState } from "react"
+import Head from "next/head"
+import Button from "../../components/Button"
+import Image from "../../components/Image"
+import QuantityPicker from "../../components/QuantityPicker"
+import { fetchInventory } from "../../utils/inventoryProvider"
+import { slugify } from "../../utils/helpers"
+import CartLink from "../../components/CartLink"
+import {
+  SiteContext,
+  ContextProviderComponent,
+} from "../../context/mainContext"
+import prismaClient from "../../lib/prisma-client"
 
 const ItemView = (props) => {
   const [numberOfitems, updateNumberOfItems] = useState(1)
   const { product } = props
-  const { price, image, name, description } = product
-  const { context: { addToCart }} = props
+  const { price, image, name, description, currentInventory } = product
+  const {
+    context: { addToCart },
+  } = props
 
-  function addItemToCart (product) {
+  function addItemToCart(product) {
     product["quantity"] = numberOfitems
     addToCart(product)
   }
 
   function increment() {
-    updateNumberOfItems(numberOfitems + 1)
+    if (numberOfitems < currentInventory) {
+      updateNumberOfItems(numberOfitems + 1)
+    }
   }
 
   function decrement() {
@@ -34,24 +42,24 @@ const ItemView = (props) => {
       <Head>
         <title>Jamstack ECommerce - {name}</title>
         <meta name="description" content={description} />
-        <meta property="og:title" content={`Jamstack ECommerce - ${name}`} key="title" />
+        <meta
+          property="og:title"
+          content={`Jamstack ECommerce - ${name}`}
+          key="title"
+        />
       </Head>
-      <div className="
-        sm:py-12
-        md:flex-row
-        py-4 w-full flex flex-1 flex-col my-0 mx-auto
-      ">
-        <div className="w-full md:w-1/2 h-120 flex flex-1 bg-light hover:bg-light-200">
-          <div className="py-16 p10 flex flex-1 justify-center items-center">
+      <div className="flex flex-col flex-1 w-full py-4 mx-auto my-0 sm:py-12 md:flex-row">
+        <div className="flex flex-1 w-full md:w-1/2 h-120 bg-light hover:bg-light-200">
+          <div className="flex items-center justify-center flex-1 py-16 p10">
             <Image src={image} alt="Inventory item" className="max-h-full" />
           </div>
         </div>
-        <div className="pt-2 px-0 md:px-10 pb-8 w-full md:w-1/2">
-          <h1 className="
-           sm:mt-0 mt-2 text-5xl font-light leading-large
-          ">{name}</h1>
-          <h2 className="text-2xl tracking-wide sm:py-8 py-6">${price}</h2>
-          <p className="text-gray-600 leading-7">{description}</p>
+        <div className="w-full px-0 pt-2 pb-8 md:px-10 md:w-1/2">
+          <h1 className="mt-2 text-5xl font-light sm:mt-0 leading-large">
+            {name}
+          </h1>
+          <h2 className="py-6 text-2xl tracking-wide sm:py-8">${price}</h2>
+          <p className="leading-7 text-gray-600">{description}</p>
           <div className="my-6">
             <QuantityPicker
               increment={increment}
@@ -70,26 +78,30 @@ const ItemView = (props) => {
   )
 }
 
-export async function getStaticPaths () {
+export async function getStaticPaths() {
   const inventory = await fetchInventory()
-  const paths = inventory.map(item => {
-    return { params: { name: slugify(item.name) }}
+  const paths = inventory.map((item) => {
+    return { params: { name: slugify(item.name) } }
   })
   return {
     paths,
-    fallback: false
+    fallback: false,
   }
 }
 
-export async function getStaticProps ({ params }) {
-  const name = params.name.replace(/-/g," ")
-  const inventory = await fetchInventory()
-  const product = inventory.find(item => slugify(item.name) === slugify(name))
-
+export async function getStaticProps({ params }) {
+  const name = params.name.replace(/-/g, " ")
+  const product = await prismaClient.product.findFirst({
+    where: {
+      name: {
+        contains: name,
+      },
+    },
+  })
   return {
     props: {
       product,
-    }
+    },
   }
 }
 
@@ -97,9 +109,7 @@ function ItemViewWithContext(props) {
   return (
     <ContextProviderComponent>
       <SiteContext.Consumer>
-        {
-          context => <ItemView {...props} context={context} />
-        }
+        {(context) => <ItemView {...props} context={context} />}
       </SiteContext.Consumer>
     </ContextProviderComponent>
   )
