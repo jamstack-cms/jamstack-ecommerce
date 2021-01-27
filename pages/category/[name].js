91acdd9a365fc1c997733357bbad981ab5f7c514
Joe Ng'ethe <joeynimu@gmail.com>
2021-01-27 19:23:31 +0300

    feature: fetch category inventory/data from the backend

--- pages/category/[name].js
+++ pages/category/[name].js
@@ -1,68 +1,76 @@
-import Head from 'next/head'
-import ListItem from '../../components/ListItem'
-import { titleIfy, slugify } from '../../utils/helpers'
-import fetchCategories from '../../utils/categoryProvider'
-import inventoryForCategory from '../../utils/inventoryForCategory'
-import CartLink from '../../components/CartLink'
+import Head from "next/head"
+import ListItem from "../../components/ListItem"
+import { titleIfy, slugify } from "../../utils/helpers"
+import fetchCategories from "../../utils/categoryProvider"
+import CartLink from "../../components/CartLink"
+import prismaClient from "../../lib/prisma-client"
 
-const Category = (props) => {
-  const { inventory, title } = props
+const Category = ({ inventory, title }) => {
   return (
     <>
       <CartLink />
       <Head>
         <title>Jamstack ECommerce - {title}</title>
         <meta name="description" content={`Jamstack ECommerce - ${title}`} />
-        <meta property="og:title" content={`Jamstack ECommerce - ${title}`} key="title" />
+        <meta
+          property="og:title"
+          content={`Jamstack ECommerce - ${title}`}
+          key="title"
+        />
       </Head>
       <div className="flex flex-col items-center">
-        <div className="max-w-fw flex flex-col w-full">
-          <div className="pt-4 sm:pt-10 pb-8">
+        <div className="flex flex-col w-full max-w-fw">
+          <div className="pt-4 pb-8 sm:pt-10">
             <h1 className="text-5xl font-light">{titleIfy(title)}</h1>
           </div>
 
           <div>
-            <div className="flex flex-1 flex-wrap flex-row">
-              {
-                inventory.map((item, index) => {
-                  return (
-                    <ListItem
-                      key={index}
-                      link={`/product/${slugify(item.name)}`}
-                      title={item.name}
-                      price={item.price}
-                      imageSrc={item.image}
-                    />
-                  )
-                })
-              }
+            <div className="flex flex-row flex-wrap flex-1">
+              {inventory.map((item, index) => {
+                return (
+                  <ListItem
+                    key={index}
+                    link={`/product/${slugify(item.name)}`}
+                    title={item.name}
+                    price={item.price}
+                    imageSrc={item.image}
+                  />
+                )
+              })}
             </div>
           </div>
-          </div>
+        </div>
       </div>
     </>
   )
 }
 
-export async function getStaticPaths () {
+export async function getStaticPaths() {
   const categories = await fetchCategories()
-  const paths = categories.map(category => {
-    return { params: { name: slugify(category) }}
+  const paths = categories.map((category) => {
+    return { params: { name: slugify(category) } }
   })
   return {
     paths,
-    fallback: false
+    fallback: false,
   }
 }
 
-export async function getStaticProps ({ params }) {
-  const category = params.name.replace(/-/g," ")
-  const inventory = await inventoryForCategory(category)
+export async function getStaticProps({ params }) {
+  const category = params.name.replace(/-/g, " ")
+  const products = await prismaClient.product.findMany({
+    where: {
+      categories: {
+        some: { name: category },
+      },
+    },
+  })
+
   return {
     props: {
-      inventory,
-      title: category
-    }
+      inventory: products,
+      title: category,
+    },
   }
 }
 
