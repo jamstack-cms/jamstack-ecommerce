634d9bdbf484b6459bdc2bf8aea5c091780fd9f1
bertrandev <bertrand.pihan@nextsourcia.com>
2021-03-08 15:43:46 +0100

    [Fix] Remove axios for category

--- provider/aquila-cms/categoryProvider.js
+++ provider/aquila-cms/categoryProvider.js
@@ -1,8 +1,11 @@
-import axios from 'axios'
-
 async function fetchCategories () {
-  const response = await axios.post('/v2/categories', {lang:"en", PostBody: {filter: {'action':'catalog'}, limit: 99}})
-  return response.data.datas.map(cat => cat.name)
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/v2/categories', {
+    method: 'POST',
+    body: JSON.stringify({lang:"en", PostBody: {"limit":99, "filter":{"code":"my-products"}, "structure":{"children":1}, "populate":["children"]}}),
+    headers: {"Content-type": "application/json; charset=UTF-8"}
+  })
+  const data = await response.json();
+  return data.datas[0].children.map(cat => cat.name)
 }
 
 export default {
